a805350c7b7a917dae393a1984ab6ef66a29d1ec
PEANUTBUTTER1001 <yangjk9203@naver.com>
2025-06-26 13:51:32 +0900

    chore: 불필요한 import문 제거

--- app/src/main/java/com/openstudy/carefull/screen/Signup.kt
+++ app/src/main/java/com/openstudy/carefull/screen/Signup.kt
@@ -9,12 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
